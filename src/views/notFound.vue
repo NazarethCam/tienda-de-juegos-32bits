fcd5fad59da5384bc9b8612b840ff72f1c86ded9
NazarethCam <naza3192@gmail.com>
2022-03-19 14:28:59 -0300

    tienda de juegos 32 bits

---
+++ src/views/notFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ERROR 404</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  font-size: 3rem;
+  margin: 14rem;
+}
+</style>
